9197e4cfa4617df8216a534bf8a81a19f06ca0bb
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2026-01-29 09:58:41 +0100

    Ajout de la searchBar

---
+++ src/app/components/searchbar/searchbar.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-searchbar',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './searchbar.html',
+  styleUrl: './searchbar.css',
+})
+export class SearchBarComponent {
+  searchTerm: string = '';
+
+  @Output() searchChange = new EventEmitter<string>();
+
+  onSearchChange() {
+    this.searchChange.emit(this.searchTerm);
+  }
+}
